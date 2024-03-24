67f128d0d178991a9373e6a906951eaa12f2c8b1
Asif-Uddin-Ahmed <asifuchchas123@gmail.com>
2024-03-25 02:14:05 +0600

    This is the initial commit after creating some necessary components
    Started building a language learning web application.

---
+++ app/(marketing)/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+      <header className=' h-20 w-full border-b-2 bg-slate-200 px-4'>
+          Header
+    </header>
+  );
+}
+
+export default Header;
