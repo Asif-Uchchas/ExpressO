9d44957b1111cd09781cc4e9d1342eeda7e96715
Asif-Uddin-Ahmed <asifuchchas123@gmail.com>
2024-03-27 03:55:44 +0600

    Created Learn page wrappers

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,8 +1,27 @@
+import FeedWrapper from '@/components/feed-wrapper'
+import StickyWrapper from '@/components/sticky-wrapper'
 import React from 'react'
+import Header from './header'
+import UserProgress from '@/components/user-progress'
+import { title } from 'process'
 
 const LearnPage = () => {
   return (
-    <div>LearnPage</div>
+    <div className=' flex flex-row-reverse gap-[48px] px-6'>
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{ title: "Spanish", imageSrc: "/bd.svg"}}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header
+          title='Spanish'
+        />
+      </FeedWrapper>
+    </div>
   )
 }
 
